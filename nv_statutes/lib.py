741cddccc8bfcf57285cf24730de716244b162c3
Robb Shecter <robb@weblaws.org>
2017-02-07 21:25:42 -0800

    refactoring

--- nv_statutes/lib.py
+++ nv_statutes/lib.py
@@ -1,23 +1,32 @@
+from collections import namedtuple
+
 import bs4
 from bs4 import BeautifulSoup
+from toolz.recipes import partitionby
 
-
-# from toolz.recipes import partitionby
+Title   = namedtuple('Title', ['name', 'number', 'chapters'])
+Chapter = namedtuple('Chapter', ['name', 'number', 'url'])
 
 
 def title_count(html_index: str) -> int:
-    soup        = BeautifulSoup(html_index, 'html.parser')
-    table       = soup.find_all('table')[1]
-    rows        = table.find_all('tr')
+    rows        = contentRows(html_index)
     header_rows = list(filter(is_header_row, rows))
     return len(header_rows)
 
 
 def titles(html_index: str):
-    soup        = BeautifulSoup(html_index, 'html.parser')
-    table       = soup.find_all('table')[1]
-    rows        = table.find_all('tr')
+    tuples      = rowTuples(contentRows(html_index))
     return []
+
+
+def contentRows(html_index: str):
+    soup  = BeautifulSoup(html_index, 'html.parser')
+    table = soup.find_all('table')[1]
+    return table.find_all('tr')
+
+
+def rowTuples(rows):
+    list(partitionby(is_header_row, rows))
 
 
 def is_header_row(row: bs4.element.Tag) -> bool:
