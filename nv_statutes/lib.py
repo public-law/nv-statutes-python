051b237be79dcdad9aedef790a041e3b4df23777
Robb Shecter <robb@weblaws.org>
2017-02-07 21:14:12 -0800

    failing test

--- nv_statutes/lib.py
+++ nv_statutes/lib.py
@@ -13,6 +13,13 @@ def title_count(html_index: str) -> int:
     return len(header_rows)
 
 
+def titles(html_index: str):
+    soup        = BeautifulSoup(html_index, 'html.parser')
+    table       = soup.find_all('table')[1]
+    rows        = table.find_all('tr')
+    return []
+
+
 def is_header_row(row: bs4.element.Tag) -> bool:
     columns = row.find_all('td')
     return len(columns) == 1
--- tests/misc_test.py
+++ tests/misc_test.py
@@ -1,6 +1,11 @@
-from nv_statutes.lib import title_count
+from nv_statutes.lib import title_count, titles
 
 
 def test_finds_the_correct_number_of_titles():
     index_html = open("../nrs.html").read()
     assert title_count(index_html) == 59
+
+
+def test_titles_gets_the_first_titles_name():
+    index_html = open("../nrs.html").read()
+    assert titles(index_html)[0].name == "STATE JUDICIAL DEPARTMENT"
